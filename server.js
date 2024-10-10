ababfeec3c8e5ec343495af08a85daf5ee10084c
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-10-10 09:26:38 +0530

    title

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error(err));
 
-
+app.get('/', (req, res) => {
+  res.send('blog backend');
+});
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
