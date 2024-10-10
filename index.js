9cc615ed756a1ea24827a793d62d629c8ff49d6c
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-10-10 09:59:53 +0530

    please

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ app.get('/', (req, res) => {
   res.send('blog backend');
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
-// module.exports = app;
+module.exports = app;
